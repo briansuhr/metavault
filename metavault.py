cf78ea6201555f73c0e43c6542f1c1493e05c3c9
Brian Suhr <brian@briansuhr.com>
2017-04-26 15:29:12 -0700

    Get IPTC keywords from image

--- metavault.py
+++ metavault.py
@@ -2,18 +2,20 @@ import os
 import pyexiv2
 
 photo_directory = 'resourcespace'
+image_file = '9944_8640aa4d8fb8168.jpg'
 
 def print_all_files():
     for sub_directory, directorys, files in os.walk(photo_directory):
         for file in files:
             print(os.path.join(sub_directory, file))
 
-def read_iptc_tags(image_file):
+def get_iptc_keywords(image_file):
     metadata = pyexiv2.ImageMetadata(image_file)
     metadata.read()
 
     for key in metadata.iptc_keys:
         tag = metadata[key]
-        print(tag)
+        if key == 'Iptc.Application2.Keywords':
+            print(tag.value)
 
-read_iptc_tags('9944_8640aa4d8fb8168.jpg')
+get_iptc_keywords(image_file)
