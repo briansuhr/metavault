24603ac4d70810afdcf98db06f1ba60f627bf569
Brian Suhr <brian@briansuhr.com>
2017-04-26 15:29:11 -0700

    Print all files in photo vault directory

--- metavault.py
+++ metavault.py
@@ -1,1 +1,10 @@
- def print_all_files():
+import os
+
+photo_directory = 'resourcespace'
+
+def print_all_files():
+    for sub_directory, directorys, files in os.walk(photo_directory):
+        for file in files:
+            print(os.path.join(sub_directory, file))
+
+print_all_files()
