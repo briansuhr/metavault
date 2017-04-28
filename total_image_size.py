35eb1462e9d8da19a92dfa27be4eb013d87a1aa9
Brian Suhr <brian@briansuhr.com>
2017-04-28 11:33:21 -0700

    Change name of image files txt

--- total_image_size.py
+++ total_image_size.py
@@ -1,5 +1,5 @@
 def get_size_of_all_images():
-    list_of_images = open('images_list.txt', 'r')
+    list_of_images = open('image_files.txt', 'r')
     for image in list_of_images():
         print(image)
 
