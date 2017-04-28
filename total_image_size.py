bf3d458dcc9567fbc10dd4288a2d773f5257da98
Brian Suhr <brian@briansuhr.com>
2017-04-28 12:37:41 -0700

    Get full image path from txt log

--- total_image_size.py
+++ total_image_size.py
@@ -3,16 +3,18 @@ def get_size_of_all_images():
     list_of_images = open('image_files.txt', 'r')
     images = list_of_images.readlines()
     for image in images:
-        image_path = replace_characters(image)
+        image_path = get_full_image_path(image)
         print(image_path)
     list_of_images.close()
 
 
-def replace_characters(image_path):
+def get_full_image_path(image_path):
     characters = "[]'"
     for character in characters:
         if character in image_path:
             image_path = image_path.replace(character, "")
+            image_path = image_path.replace(',', '/')
+            image_path = image_path.replace(' ', '')
     return(image_path)
 
 get_size_of_all_images()
