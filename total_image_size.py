c87db687b294baad538ff57a1368ac8e018c8e22
Brian Suhr <brian@briansuhr.com>
2017-04-28 11:42:08 -0700

    Print file directory of each image

--- total_image_size.py
+++ total_image_size.py
@@ -1,6 +1,8 @@
 def get_size_of_all_images():
     list_of_images = open('image_files.txt', 'r')
-    print(list_of_images.readlines())
+    images = list_of_images.readlines())
+    for image in images:
+        print(image[0])
     list_of_images.close()
 
 get_size_of_all_images()
