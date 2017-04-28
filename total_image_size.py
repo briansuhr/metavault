a176668385590c2fdddb6405ce0ad1ffe606720a
Brian Suhr <brian@briansuhr.com>
2017-04-28 11:34:44 -0700

    Add readlines

--- total_image_size.py
+++ total_image_size.py
@@ -1,6 +1,6 @@
 def get_size_of_all_images():
     list_of_images = open('image_files.txt', 'r')
-    for image in list_of_images():
-        print(image)
+    images = list_of_images.readlines()
+    f.close()
 
 get_size_of_all_images()
