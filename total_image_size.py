18d15d55cd738faeae638779747a4f25c726d640
Brian Suhr <brian@briansuhr.com>
2017-04-28 11:35:27 -0700

    Fix wrong name of image variable

--- total_image_size.py
+++ total_image_size.py
@@ -1,6 +1,6 @@
 def get_size_of_all_images():
     list_of_images = open('image_files.txt', 'r')
     images = list_of_images.readlines()
-    f.close()
+    images.close()
 
 get_size_of_all_images()
