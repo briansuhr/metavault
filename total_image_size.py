f5d046a5e5de6ee84cf1bcd58e349ec44435f8c0
Brian Suhr <brian@briansuhr.com>
2017-04-28 12:28:40 -0700

    Remove list formatting from text file

--- total_image_size.py
+++ total_image_size.py
@@ -1,8 +1,18 @@
+
 def get_size_of_all_images():
     list_of_images = open('image_files.txt', 'r')
-    images = list_of_images.readlines())
+    images = list_of_images.readlines()
     for image in images:
-        print(image[0])
+        image_path = replace_characters(image)
+        print(image_path)
     list_of_images.close()
+
+
+def replace_characters(image_path):
+    characters = "[]'"
+    for character in characters:
+        if character in image_path:
+            image_path = image_path.replace(character, "")
+    return(image_path)
 
 get_size_of_all_images()
