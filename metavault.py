f3583aea28b3e795e917bf1a3b193c3e6f418a34
Brian Suhr <brian@briansuhr.com>
2017-05-02 17:26:35 -0700

    Reformat with PEP8

--- metavault.py
+++ metavault.py
@@ -14,18 +14,19 @@ def get_iptc_keywords(image_file):
         if key == 'Iptc.Application2.Keywords':
             print(tag.value)
 
+
 def get_iptc_credit(image_file):
     metadata = pyexiv2.ImageMetadata(image_file)
     metadata.read()
 
     for key in metadata.iptc_keys:
         tag = metadata[key]
-        print(str(key)+ str(tag.value))
+        print(str(key) + str(tag.value))
         if key == 'Iptc.Application2.Credit':
             print(tag.value)
 
-def read_metadump(metadump_file):
 
+def read_metadump(metadump_file):
     xml_file = open(metadump_file, "r").read()
     parsed_xml_file = xmltodict.parse(xml_file)
 
@@ -76,10 +77,12 @@ def read_metadump(metadump_file):
 
     return metadata
 
+
 def is_thumbnail(image):
     # Use glob.iglob instead of glob.glob to avoid storing all files simultaneously.
     if 'col_' in image or 'lpr_' in image or 'pre_' in image or 'scr_' in image or 'thm_' in image:
         return True
+
 
 def get_image_files(image_type):
     image_directory = 'filestorebk'
@@ -95,7 +98,7 @@ def get_image_files(image_type):
 
             print(str(number_of_images) + " full size images found.", end="\r")
 
-        else:
+            else:
             number_of_images += 1
 
             # Split up directory and image basename
@@ -111,102 +114,105 @@ def get_image_files(image_type):
 
             print(str(number_of_images) + " thumbnail images found.", end="\r")
 
-    return image_files
+            return image_files
 
-def images_log(image_file_list):
-    image_files = open('image_files.txt', 'w')
-    for image_file in image_file_list:
-        image_files.write("%s\n" % image_file)
+        def images_log(image_file_list):
+            image_files = open('image_files.txt', 'w')
+            for image_file in image_file_list:
+                image_files.write("%s\n" % image_file)
 
-def write_iptc_data():
+        def write_iptc_data():
 
-    count = 0
+            count = 0
 
-    all_image_files = get_all_image_files()
-    images_log(all_image_files)
+            all_image_files = get_all_image_files()
+            images_log(all_image_files)
 
-    print("Writing metadata to images...")
+            print("Writing metadata to images...")
 
-    for image_file in all_image_files:
+            for image_file in all_image_files:
 
-        # Look for metadump file
-        metadump_file = (str(image_file[0]) + '/metadump.xml')
-        image_filepath = str(image_file[0]) + "/" + str(image_file[1])
+                # Look for metadump file
+                metadump_file = (str(image_file[0]) + '/metadump.xml')
+                image_filepath = str(image_file[0]) + "/" + str(image_file[1])
 
-        try:
-            os.path.isfile(metadump_file)
-        except:
-            print("No metadump file found for" + str(image_file[1]) + ".")
-            count += 1
-            continue
+                try:
+                    os.path.isfile(metadump_file)
+                except:
+                    print("No metadump file found for" + str(image_file[1]) + ".")
+                    count += 1
+                    continue
 
-        image_fields = pyexiv2.ImageMetadata(image_filepath)
+                image_fields = pyexiv2.ImageMetadata(image_filepath)
 
-        try:
-            image_fields.read()
-        except:
-            print("Failed to open image " + str(image_filepath))
+                try:
+                    image_fields.read()
+                except:
+                    print("Failed to open image " + str(image_filepath))
 
-            continue
+                    continue
 
-        try:
-            metadata = read_metadump(metadump_file)
-        except:
-            print("Failed to open metadump file for image " + str(image_filepath))
-            continue
+                try:
+                    metadata = read_metadump(metadump_file)
+                except:
+                    print("Failed to open metadump file for image " + str(image_filepath))
+                    continue
 
-        # Write keywords
-        try:
-            if metadata.get('Keywords'):
-                key = 'Iptc.Application2.Keywords'
-                value = metadata.get('Keywords')
-                image_fields[key] = [value]
-        except:
-            pass
+                # Write keywords
+                try:
+                    if metadata.get('Keywords'):
+                        key = 'Iptc.Application2.Keywords'
+                        value = metadata.get('Keywords')
+                        image_fields[key] = [value]
+                except:
+                    pass
 
-        # Write headline
-        try:
-            if metadata.get('Title'):
-                # Needs to be IPTC Byline. Using Creator causes an error.
-                key = 'Iptc.Application2.Headline'
-                value = metadata.get('Title')
-                image_fields[key] = [value]
-        except:
-            pass
+                # Write headline
+                try:
+                    if metadata.get('Title'):
+                        # Needs to be IPTC Byline. Using Creator causes an error.
+                        key = 'Iptc.Application2.Headline'
+                        value = metadata.get('Title')
+                        image_fields[key] = [value]
+                except:
+                    pass
 
-        # Write credit
-        try:
-            if metadata.get('Credit'):
-                # Needs to be IPTC Byline. Using Creator causes an error.
-                key = 'Iptc.Application2.Byline'
-                value = metadata.get('Byline')
-                image_fields[key] = [value]
-        except:
-            pass
+                # Write credit
+                try:
+                    if metadata.get('Credit'):
+                        # Needs to be IPTC Byline. Using Creator causes an error.
+                        key = 'Iptc.Application2.Byline'
+                        value = metadata.get('Byline')
+                        image_fields[key] = [value]
+                except:
+                    pass
 
-        try:
-            print("(" + str(int(count/len(all_image_files) * 100)) + "%) Writing metadata to image " + str(count) + " of " + str(len(all_image_files)), end="\r")
-            image_fields.write()
-            count += 1
+                try:
+                    print("(" + str(int(count / len(all_image_files) * 100)) + "%) Writing metadata to image " + str(
+                        count) + " of " + str(len(all_image_files)), end="\r")
+                    image_fields.write()
+                    count += 1
 
-        except:
-            print("Failed to add metadata to " + str(image_file[1]))
-            count += 1
-            continue
+                except:
+                    print("Failed to add metadata to " + str(image_file[1]))
+                    count += 1
+                    continue
 
-    print("\n")
-    print("Done.")
+            print("\n")
+            print("Done.")
 
-def copy_images_to_new_directory():
-    images_to_move = get_all_image_files()
-    image_destination_directory = "images_with_metadata"
+        def copy_images_to_new_directory():
+            images_to_move = get_all_image_files()
+            image_destination_directory = "images_with_metadata"
 
-    count = 0
-    for image in images_to_move:
-        count += 1
-        image_source = str(image[0]) + "/" + str(image[1])
-        image_destination_filepath = image_destination_directory + "/" + str(image[1])
-        print("(" + str(int(count/len(images_to_move) * 100)) + "%) Copying image " + str(count) + " of " + str(len(images_to_move)) + " to /" + image_destination_directory, end="\r")
-        copyfile(image_source, image_destination_filepath)
+            count = 0
+            for image in images_to_move:
+                count += 1
+                image_source = str(image[0]) + "/" + str(image[1])
+                image_destination_filepath = image_destination_directory + "/" + str(image[1])
+                print(
+                "(" + str(int(count / len(images_to_move) * 100)) + "%) Copying image " + str(count) + " of " + str(
+                    len(images_to_move)) + " to /" + image_destination_directory, end="\r")
+                copyfile(image_source, image_destination_filepath)
 
-    print("Done.")
+            print("Done.")
--- total_image_size.py
+++ total_image_size.py
@@ -3,8 +3,8 @@ from math import log
 from metavault import get_image_files
 import glob
 
-def get_size_of_images(images):
 
+def get_size_of_images(images):
     total_file_size = 0
 
     print('\n')
@@ -15,33 +15,31 @@ def get_size_of_images(images):
         total_file_size += file_size
         print(pretty_size(total_file_size), end="\r")
 
-    print("\n")
-    print("Total size: " + str(pretty_size(total_file_size)))
-    return total_file_size
+        print("\n")
+        print("Total size: " + str(pretty_size(total_file_size)))
+        return total_file_size
 
-def get_full_image_path(image_path):
+    def get_full_image_path(image_path):
 
-    characters = "[]'"
-    for character in characters:
-        if character in image_path:
-            image_path = image_path.replace(character, "")
-            image_path = image_path.replace(',', '/')
-            image_path = image_path.replace(' ', '')
-            image_path = image_path.strip()
-        print(image_path)
-    return image_path
+        characters = "[]'"
+        for character in characters:
+            if character in image_path:
+                image_path = image_path.replace(character, "")
+                image_path = image_path.replace(',', '/')
+                image_path = image_path.replace(' ', '')
+                image_path = image_path.strip()
+            print(image_path)
+        return image_path
 
-def get_thumbnails_path(image_path):
-    thumbnails_path = []
+    def get_thumbnails_path(image_path):
+        thumbnails_path = []
 
-    thumbnail_prefixes = ['col_', 'lpr_', 'pre_', 'scr_', 'thm_']
-    for prefix in thumbnail_prefixes:
-        thumbnails_path.append()
+        thumbnail_prefixes = ['col_', 'lpr_', 'pre_', 'scr_', 'thm_']
+        for prefix in thumbnail_prefixes:
+            thumbnails_path.append()
 
+    def pretty_size(n, pow=0, b=1024, u='B', pre=[''] + [p + 'i' for p in 'KMGTPEZY']):
+        pow, n = min(int(log(max(n * b ** pow, 1), b)), len(pre) - 1), n * b ** pow
+        return "%%.%if %%s%%s" % abs(pow % (-pow - 1)) % (n / b ** float(pow), pre[pow], u)
 
-def pretty_size(n,pow=0,b=1024,u='B',pre=['']+[p+'i'for p in'KMGTPEZY']):
-    pow,n=min(int(log(max(n*b**pow,1),b)),len(pre)-1),n*b**pow
-    return "%%.%if %%s%%s"%abs(pow%(-pow-1))%(n/b**float(pow),pre[pow],u)
-
-
-get_size_of_images(get_image_files("thumbnail"))
+    get_size_of_images(get_image_files("thumbnail"))
