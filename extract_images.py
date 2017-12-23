7764f1879c9e00b52e77d960eec5def0609eba31
Brian Suhr <brian@briansuhr.com>
2017-12-22 19:31:48 -0800

    Count images in image log

--- extract_images.py
+++ extract_images.py
@@ -1,6 +1,16 @@
 from shutil import copyfile
 import os
 
+
+def count_total_images(image_log):
+    image_count = 0
+    with open(image_log) as image_log:
+        images = image_log.readlines()
+        for image in images:
+            count += 1
+
+    return image_count
+
 def extract_images(image_log):
     """Uses the image log to move all full-size images to a new directory"""
 
@@ -10,9 +20,11 @@ def extract_images(image_log):
 
     for image in images:
         base_image_path = os.path.basename(image)
-        extraction_directory = os.path.join("puddy", base_image_path)
+        extraction_directory = os.path.join("extract", base_image_path)
 
         copyfile(image, extraction_directory)
 
 
-extract_images("log/image_files.txt")
+if __name__ == "__main__":
+    print(count_total_images("log/image_files.txt"))
+    extract_images("log/image_files.txt")
