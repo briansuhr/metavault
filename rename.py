65389d31ae2f5d12a08935f1446d51c7621ca84a
Brian Suhr <brian@briansuhr.com>
2017-12-22 18:40:50 -0800

    Recursively rename all images with orig filename

--- rename.py
+++ rename.py
@@ -15,6 +15,22 @@ def rename_photos():
         else:
             continue
 
+def list_all_photos():
+    image_directory = "photos/"
+    for root_directory, sub_directories, images in os.walk(image_directory):
+        for image in images:
+            if image.startswith("._"):
+                continue
+            elif is_thumbnail(image):
+                continue
+            elif image.endswith(".jpg") or image.endswith('.jpeg') or image.endswith(".tif"):
+                image_path = os.path.join(root_directory, image)
+                metadump_file_location = os.path.join(root_directory, "metadump.xml")
+                original_filename = get_xml(metadump_file_location)['Original filename']
+                os.rename(image_path, os.path.join(root_directory, original_filename))
+            else:
+                continue
+
 if __name__ == '__main__':
 
-    rename_photos()
+    list_all_photos()
