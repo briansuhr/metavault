7e9c6955dc00297b39a4feae0e726e65d71856a4
Brian Suhr <brian@briansuhr.com>
2017-12-22 17:32:05 -0800

    Rename variable for consistency

---
+++ find_images.py
@@ -0,0 +1,37 @@
+import os
+import glob
+from photo_details import is_thumbnail, get_xml
+
+
+def find_image_files(image_type):
+    image_directory = 'filestorebk'
+    number_of_images = 0
+    image_files = []
+
+    for image in glob.iglob(image_directory + '/**/*.jpg', recursive=True):
+
+        # Use glob.iglob instead of glob.glob to avoid storing all files simultaneously.
+
+        if image_type == "original" and not is_thumbnail(image):
+            number_of_images += 1
+
+            get_xml(image)
+
+            print(str(number_of_images) + " full size images found.", end="\r")
+
+        elif image_type == "thumbnail" and is_thumbnail(image):
+            number_of_images += 1
+
+            # Split up directory and image basename
+            image_file_location = []
+            image_directory = os.path.dirname(image)
+            image_basename = os.path.basename(image)
+
+            image_file_location.append(image_directory)
+            image_file_location.append(image_basename)
+
+            image_files.append(image_file_location)
+
+            print(str(number_of_images) + " thumbnail images found.", end="\r")
+
+        return image_files
--- rename.py
+++ rename.py
@@ -4,14 +4,14 @@ from photo_details import get_xml, is_thumbnail
 def rename_photos():
     """Renames photos in directory with XML metadump information"""
 
-    photo_directory = "photos/7_17dae6df35e1463/"
-    for photo in os.listdir(photo_directory):
+    image_directory = "photos/7_17dae6df35e1463/"
+    for photo in os.listdir(image_directory):
         if photo.startswith("._"):
             continue
         elif is_thumbnail(photo):
             continue
         elif photo.endswith(".jpg") or photo.endswith('.jpeg') or photo.endswith(".tif"):
-            os.rename(photo_directory + photo, photo_directory + get_xml(photo_directory + "metadump.xml")['Original filename'])
+            os.rename(image_directory + photo, image_directory + get_xml(image_directory + "metadump.xml")['Original filename'])
         else:
             continue
 
