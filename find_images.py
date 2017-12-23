d19e05e5176155389f31b7c0d6d4bb30837edd6c
Brian Suhr <brian@briansuhr.com>
2017-12-22 19:00:17 -0800

    Create log of image files

--- find_images.py
+++ find_images.py
@@ -2,36 +2,37 @@ import os
 import glob
 from image_details import is_thumbnail, get_xml
 
+def find_all_images(image_directory):
+    """Recursively searches a photo directory for all non-thumbnail files"""
 
-def find_image_files(image_type):
-    image_directory = 'filestorebk'
-    number_of_images = 0
+    images_found = 0
     image_files = []
 
-    for image in glob.iglob(image_directory + '/**/*.jpg', recursive=True):
+    for root_directory, sub_directories, images in os.walk(image_directory):
+        for image in images:
+            if image.startswith("._"):
+                continue
+            elif is_thumbnail(image):
+                continue
+            elif image.endswith(".jpg") or image.endswith('.jpeg') or image.endswith(".tif"):
+                image_path = os.path.join(root_directory, image)
+                images_found += 1
+                image_files.append(image_path)
+            else:
+                continue
 
-        # Use glob.iglob instead of glob.glob to avoid storing all files simultaneously.
+        print(str(images_found) + " images found.", end="\r")
 
-        if image_type == "original" and not is_thumbnail(image):
-            number_of_images += 1
+    print("\nDone.")
+    return image_files
 
-            get_xml(image)
 
-            print(str(number_of_images) + " full size images found.", end="\r")
+def create_log(image_directory):
+    """Creates log containing the paths of all image files"""
+    image_files = open('log/image_files.txt', 'w')
+    for path in find_all_images(image_directory):
+        image_files.write("%s\n" % path)
 
-        elif image_type == "thumbnail" and is_thumbnail(image):
-            number_of_images += 1
 
-            # Split up directory and image basename
-            image_file_location = []
-            image_directory = os.path.dirname(image)
-            image_basename = os.path.basename(image)
-
-            image_file_location.append(image_directory)
-            image_file_location.append(image_basename)
-
-            image_files.append(image_file_location)
-
-            print(str(number_of_images) + " thumbnail images found.", end="\r")
-
-        return image_files
+if __name__ == "__main__":
+    create_log("photos/")
