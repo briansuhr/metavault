edf14fc69ef50b648b42ceb324454af573fc392c
Brian Suhr <brian@briansuhr.com>
2017-12-22 19:38:32 -0800

    Display progress

--- extract_images.py
+++ extract_images.py
@@ -2,12 +2,11 @@ from shutil import copyfile
 import os
 
 
-def count_total_images(image_log):
+def count_total_images(images):
     image_count = 0
-    with open(image_log) as image_log:
-        images = image_log.readlines()
-        for image in images:
-            count += 1
+
+    for image in images:
+        image_count += 1
 
     return image_count
 
@@ -17,14 +16,18 @@ def extract_images(image_log):
     with open(image_log) as image_log:
         images = image_log.readlines()
         images = [white_space.strip() for white_space in images]
+        total_images = count_total_images(images)
 
+    count = 1
     for image in images:
+        print("Copying image " + str(count) + " of " + str(total_images) + ".", end="\r")
         base_image_path = os.path.basename(image)
         extraction_directory = os.path.join("extract", base_image_path)
-
         copyfile(image, extraction_directory)
+        count += 1
+
+    print("\nDone.")
 
 
 if __name__ == "__main__":
-    print(count_total_images("log/image_files.txt"))
     extract_images("log/image_files.txt")
