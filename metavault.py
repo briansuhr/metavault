9a1371870edb7a475c969aa59689bd8340bb4c11
Brian Suhr <brian@briansuhr.com>
2017-12-22 19:06:03 -0800

    Remove image find from metavault

--- metavault.py
+++ metavault.py
@@ -5,41 +5,6 @@ from shutil import copyfile
 
 
 def get_image_files(image_type):
-    image_directory = 'filestorebk'
-    number_of_images = 0
-    image_files = []
-
-    for image in glob.iglob(image_directory + '/**/*.jpg', recursive=True):
-        # Use glob.iglob instead of glob.glob to avoid storing all files simultaneously.
-        if image_type == "original" and is_thumbnail(image) == False:
-            number_of_images += 1
-
-            get_image_file_details(image)
-
-            print(str(number_of_images) + " full size images found.", end="\r")
-
-            else:
-            number_of_images += 1
-
-            # Split up directory and image basename
-            image_file_location = []
-            image_directory = os.path.dirname(image)
-            image_basename = os.path.basename(image)
-
-            image_file_location.append(image_directory)
-            image_file_location.append(image_basename)
-
-            image_files.append(image_file_location)
-            image_file_location = []
-
-            print(str(number_of_images) + " thumbnail images found.", end="\r")
-
-            return image_files
-
-        def images_log(image_file_list):
-            image_files = open('image_files.txt', 'w')
-            for image_file in image_file_list:
-                image_files.write("%s\n" % image_file)
 
         def write_iptc_data():
 
